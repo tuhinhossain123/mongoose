2cb61a07a81264bfc4af4e94734c51ea90d2b442
tuhinhossain123 <csetuhin55@gmail.com>
2024-10-26 09:21:03 +0600

    shf

--- src/app/modules/student/student.route.ts
+++ src/app/modules/student/student.route.ts
@@ -4,6 +4,6 @@ import { StudentsControllers } from './student.controller';
 const router = express.Router();
 router.post('create-student', StudentsControllers.createStudent);
 
-router.get('/', StudentsControllers.getAllStudents)
+router.get('/', StudentsControllers.getAllStudents);
 
 export const studentRoutes = router;
